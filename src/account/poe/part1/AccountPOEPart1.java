2670a26ea968ee29b0986b60c99ed2c462c4a03c
Samukelo16 <raizorsteazy@gmail.com>
2026-04-02 09:38:48 +0200

    Update AccountPOEPart1.java

--- src/account/poe/part1/AccountPOEPart1.java
+++ src/account/poe/part1/AccountPOEPart1.java
@@ -3,17 +3,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package account.poe.part1;
-
+import java.util.Scanner;
 /**
  *
  * @author Student
  */
 public class AccountPOEPart1 {
+    
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner username = new Scanner(System.in);
+        // USERNAME
+        String username =input.nextLine();
+        
+        if (username contains("_") && usename.length() <= 5){
+           System.out.println("Username successfully captured.");
+        } else if{
+            System.out.println("Username is not correctly formatted;Please ensure your username contains a underscore and is no more than five charactors in lenth");
+                }   
+        
         // TODO code application logic here
     }
     
