751415e61cdbca7906cf7b35ac7b6726dfc299f5
Samukelo16 <raizorsteazy@gmail.com>
2026-04-03 11:37:03 +0200

    Adding modifiers to my code, both private and public

--- src/account/poe/part1/AccountPOEPart1.java
+++ src/account/poe/part1/AccountPOEPart1.java
@@ -3,27 +3,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package account.poe.part1;
-import java.util.Scanner;
+
 /**
  *
  * @author Student
  */
 public class AccountPOEPart1 {
+    private String username;
+    private String password;
+    private String phoneNumber;
     
+    public Login(String username, String password, String phoneNumber){
+        this.username = username;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner username = new Scanner(System.in);
-        // USERNAME
-        String username =input.nextLine();
-        
-        if (username contains("_") && usename.length() <= 5){
-           System.out.println("Username successfully captured.");
-        } else if{
-            System.out.println("Username is not correctly formatted;Please ensure your username contains a underscore and is no more than five charactors in lenth");
-                }   
+         
         
         // TODO code application logic here
     }
